be354275cc38551ae0dc4c689de42c9dc6e940a7
Andy <aking27@wisc.edu>
2020-12-04 16:15:16 -0600

    Started work on Main Activity

--- app/src/main/java/com/example/gymbro/MainActivity.java
+++ app/src/main/java/com/example/gymbro/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.gymbro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button addDay;
+    Button profile;
+    Button settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addDay = (Button) findViewById(R.id.addDay);
+        profile = (Button) findViewById(R.id.profile);
+        settings = (Button) findViewById(R.id.settings);
+
+//        addDay.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                startActivity(new Intent(getApplicationContext(), ClientUserActivity.class));
+//            }
+//        });
     }
 }
